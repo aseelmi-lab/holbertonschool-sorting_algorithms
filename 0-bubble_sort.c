a7390a510cbf100ba3d2bfe1ca09704cea05722e
aseelmi-lab <aseel.mashall@gmail.com>
2026-04-30 20:12:27 +0000

    Fix bubble sort swap logic and O file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,21 +2,23 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order
- * @array: The array to be sorted
- * @size: Number of elements in @array
+ * using the Bubble sort algorithm.
+ * @array: The array to be sorted.
+ * @size: Number of elements in the array.
+ *
+ * Description: Swaps adjacent elements if they are in the wrong order
+ * and prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int swapped;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,11 +26,9 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				/* Print array after each swap to meet project requirements */
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-		if (swapped == 0)
-			break;
 	}
 }
--- sort.h
+++ sort.h
@@ -6,6 +6,7 @@
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -17,8 +18,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Functions provided for the project */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/* Task 0 */
 void bubble_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
