ec9b9287afd8e9b89a8eea1397c9bd4a9e67156b
rywma <almujallireema@gmail.com>
2026-05-09 12:11:38 +0000

    Add selection sort imp

--- sort.h
+++ sort.h
@@ -28,4 +28,6 @@ void bubble_sort(int *array, size_t size);
 /* Task 1: Insertion Sort */
 void insertion_sort_list(listint_t **list);
 
+/* Task 2: Selection sort */
+void selection_sort(int *array, size_t size);
 #endif /* SORT_H */
